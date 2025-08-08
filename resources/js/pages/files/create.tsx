7896b50f57c4a782e9e7be5ded5fc48eb9387054
amos-babu <amos.babu@yahoo.com>
2025-08-08 15:42:10 +0300

    'types'

--- resources/js/pages/files/create.tsx
+++ resources/js/pages/files/create.tsx
@@ -4,8 +4,28 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import AppLayout from '@/layouts/app-layout';
 import { BreadcrumbItem } from '@/types';
+import { router, useForm } from '@inertiajs/react';
+import React from 'react';
+
+type CreateProps = {
+    name: string;
+    description: string;
+    image: File | null;
+    dxf_file: File | null;
+};
 
 export default function Create() {
+    const { data, setData, post, errors } = useForm<CreateProps>({
+        name: '',
+        description: '',
+        image: null,
+        dxf_file: null,
+    });
+
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        router.post(route('files.store'), data);
+    };
     const breadcrumbs: BreadcrumbItem[] = [
         {
             title: 'Upload File',
@@ -14,23 +34,48 @@ export default function Create() {
     ];
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <form>
+            <form onSubmit={submit} className="md:w-3/4 lg:w-1/2">
                 <h1 className="my-7 text-center text-lg font-bold">Upload your file here</h1>
                 <div className="px-6 py-3">
                     <Label htmlFor="name">Name</Label>
-                    <Input type="text" placeholder="Name" className="mt-3" />
+                    <Input
+                        value={data.name}
+                        onChange={(e) => setData('name', e.target.value)}
+                        name="name"
+                        type="text"
+                        placeholder="Name"
+                        className="mt-3"
+                    />
                 </div>
                 <div className="px-6 py-3">
                     <Label htmlFor="name">Description</Label>
-                    <Textarea className="mt-3" placeholder="Type your description here.." />
+                    <Textarea
+                        value={data.description}
+                        onChange={(e) => setData('description', e.target.value)}
+                        name="description"
+                        className="mt-3"
+                        placeholder="Type your description here.."
+                    />
                 </div>
                 <div className="px-6 py-3">
                     <Label htmlFor="name">File Image</Label>
-                    <Input id="picture" type="file" className="mt-3" />
+                    <Input
+                        onChange={(e) => setData('image', e.target.files ? e.target.files[0] : null)}
+                        name="image"
+                        id="picture"
+                        type="file"
+                        className="mt-3"
+                    />
                 </div>
                 <div className="px-6 py-3">
                     <Label htmlFor="name">Dxf File</Label>
-                    <Input id="picture" type="file" className="mt-3" />
+                    <Input
+                        onChange={(e) => setData('dxf_file', e.target.files ? e.target.files[0] : null)}
+                        name="dxf_file"
+                        id="picture"
+                        type="file"
+                        className="mt-3"
+                    />
                 </div>
                 <div className="px-6 pt-3">
                     <Button type="submit">Submit</Button>
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -41,3 +41,10 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+export interface CreateProps {
+    name: string;
+    description: string;
+    image: File;
+    dxf_file: File;
+}
