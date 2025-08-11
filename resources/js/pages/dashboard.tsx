ffe56d3ca6477ef458680fdf771e89bf1906ff11
Amos Babu <amos.babu@yahoo.com>
2025-08-11 22:58:15 +0300

    use view pagination configuration

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import { AspectRatio } from '@/components/ui/aspect-ratio';
 import AppLayout from '@/layouts/app-layout';
 import { FileProps, FlashProps, type BreadcrumbItem } from '@/types';
-import { Head, Link, usePage } from '@inertiajs/react';
+import { Head, Link, router, usePage } from '@inertiajs/react';
 import { useEffect, useState } from 'react';
 import { toast, Toaster } from 'sonner';
 
@@ -14,7 +14,30 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 export default function Dashboard({ files }: { files: FileProps }) {
     const { flash } = usePage<FlashProps>().props;
+    const [items, setItems] = useState(files.data)
+    const [nextPage, setNextPage] = useState(files.links.next)
     const [successMessage, setSuccessMessage] = useState(flash.success);
+
+    useEffect(() => {
+        setItems(files.data)
+        setNextPage(files.links.next)
+    }, [files])
+
+    const loadMore = () => {
+        if (!nextPage) return
+
+        router.visit(nextPage, {
+            preserveScroll: true,
+            preserveState: true,
+            replace: true,
+            only: ['files'],
+            onSuccess: (page) => {
+                setItems(prev => [...prev, ...page.props.files.data ])
+                setNextPage(page.props.files.links.next);
+            }
+        });
+    };
+
 
     useEffect(() => {
         if (successMessage) {
@@ -29,7 +52,7 @@ export default function Dashboard({ files }: { files: FileProps }) {
             <Toaster richColors position="top-center" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    {files.data.map((file) => (
+                    {items.map((file) => (
                         <Link key={file.id} href={route('files.show', file)}>
                             <AspectRatio ratio={15 / 9} className="rounded-lg bg-muted">
                                 <img
