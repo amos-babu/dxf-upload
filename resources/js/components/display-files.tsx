f8b859a06972afae3c62be8078eba34e441580ab
Babu Amos <amos.babu@yahoo.com>
2025-09-09 00:14:40 +0300

    auth split layout

--- resources/js/components/display-files.tsx
+++ resources/js/components/display-files.tsx
@@ -1,22 +1,26 @@
 import { FileDataProps } from '@/types';
 import { Link } from '@inertiajs/react';
-import Masonry from 'react-masonry-css';
 
 export default function DisplayFiles({ items }: { items: FileDataProps[] }) {
     const breakpointColumnsObj = {
-        default: 4,
+        default: 3,
         1100: 3,
         700: 2,
         500: 1,
     };
     return items.map((file, idx) => (
-        <Masonry className="my-masonry-grid" columnClassName="my-masonry-grid_column" key={`${file.id}-${idx}`} breakpointCols={breakpointColumnsObj}>
-            <Link href={route('files.show', { file: file.id })}>
-                <img className="h-auto max-w-full rounded-2xl" src={file.image} alt={file.name}></img>
-                {/* <AspectRatio ratio={15 / 9} className="rounded-lg bg-muted">
+        // <Masonry className="my-masonry-grid" columnClassName="my-masonry-grid_column" key={`${file.id}-${idx}`} breakpointCols={breakpointColumnsObj}>
+        <Link key={`${file.id}-${idx}`} href={route('files.show', { file: file.id })}>
+            {/* <img className="h-auto max-w-full rounded-2xl" src={file.image} alt={file.name}></img> */}
+            {/* <AspectRatio ratio={15 / 9} className="rounded-lg bg-muted">
                 <img src={file.image} alt={file.name} className="h-full w-full rounded-lg object-cover" />
             </AspectRatio> */}
-            </Link>
-        </Masonry>
+
+            <figure className="max-w-lg rounded-lg bg-muted p-3 transition-transform duration-300 hover:scale-105 hover:rounded-2xl hover:shadow-lg">
+                <img className="h-auto max-w-full rounded-md" src={file.image} alt={file.name} />
+                <figcaption className="mt-2 text-center text-sm text-gray-500 dark:text-gray-400">{file.name}</figcaption>
+            </figure>
+        </Link>
+        // </Masonry>
     ));
 }
--- resources/js/pages/files/show.tsx
+++ resources/js/pages/files/show.tsx
@@ -2,7 +2,6 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import AppLayout from '@/layouts/app-layout';
 import { BreadcrumbItem, ShowFileDataProps } from '@/types';
-import { AspectRatio } from '@radix-ui/react-aspect-ratio';
 
 export default function Show({ file }: { file: ShowFileDataProps }) {
     const breadcrumbs: BreadcrumbItem[] = [
@@ -13,10 +12,19 @@ export default function Show({ file }: { file: ShowFileDataProps }) {
     ];
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <div className="mt-10 flex flex-col justify-center gap-5">
-                <AspectRatio ratio={16 / 15} className="rounded-lg bg-muted">
-                    <img src={file.data.image} alt={file.data.name} className="h-full w-full rounded-lg object-cover" />
-                </AspectRatio>
+            <div className="mt-10 flex w-full flex-col justify-center gap-5 md:flex-row">
+                {/* <AspectRatio ratio={12 / 6} className="rounded-lg bg-muted">
+                        <img
+                            src={file.data.image}
+                            alt={file.data.name}
+                            className="h-full w-full rounded-lg border object-cover shadow-lg transition-transform duration-300 hover:scale-105"
+                        />
+                    </AspectRatio> */}
+
+                {/* <figure className="bg-muted">
+                    <img className="h-auto rounded-md" src={file.data.image} alt={file.data.name} />
+                    <figcaption className="mt-2 text-center text-sm text-gray-500 dark:text-gray-400">{file.data.name}</figcaption>
+                </figure> */}
                 <Card>
                     <CardHeader>
                         <CardTitle>{file.data.name}</CardTitle>
@@ -25,7 +33,7 @@ export default function Show({ file }: { file: ShowFileDataProps }) {
                     <CardContent>
                         <p>{file.data.description}</p>
                     </CardContent>
-                    <CardFooter className='flex gap-5'>
+                    <CardFooter className="flex gap-5">
                         <Button>Download Image</Button>
                         <Button>Download DXF</Button>
                     </CardFooter>
