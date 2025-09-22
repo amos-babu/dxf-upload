53f8196f7cb9895441fdf99ac61505b0a37c863b
amos-babu <amos.babu@yahoo.com>
2025-09-22 11:27:46 +0300

    "admin Middleware"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FileController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,7 +12,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 Route::resource('files', FileController::class)->only(["show"]);
 Route::get('/', [FileController::class, 'index'])->name('files.index');
 Route::get('/image/{file}/download/', [FileController::class, 'imageDownload'])->name('image.download');
-Route::get('/dxf/{file}/download/', [FileController::class, 'dxfDownload'])->name('dxf.download');
+Route::get('/dxf/{file}/download/', [FileController::class, 'dxfDownload'])->name('dxf.download')->middleware(AdminMiddleware::class);
 
 Route::fallback(function () {
     return Inertia::render('notfound');
