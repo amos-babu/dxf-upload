02bb45d374db8257530a2d445bb23b16d77c3cd6
amos-babu <amos.babu@yahoo.com>
2025-09-22 10:21:46 +0300

    Orderstatus enum

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -2,7 +2,17 @@
 
 namespace App\Enums;
 
-enum OrderStatus
+enum OrderStatus: string
 {
-    //
+    case PENDING = 'pending';
+    case PAID = 'paid';
+    case FAILED = 'failed';
+
+    public function label(): string {
+        return match($this){
+            self::PENDING => 'Pending',
+            self::PAID => 'Paid',
+            self::FAILED => 'Failed'
+        };
+    }
 }
