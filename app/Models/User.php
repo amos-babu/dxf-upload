a80fb26684528b7bf179865c1492717fedb8f52b
amos-babu <amos.babu@yahoo.com>
2025-09-22 10:22:43 +0300

    Userrole enum

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -23,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -45,10 +47,15 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => UserRole::class
         ];
     }
 
     public function files(): HasMany {
         return $this->hasMany(File::class);
+    }
+
+    public function isAdmin(): bool {
+        return $this->role === UserRole::ADMIN->value;
     }
 }
