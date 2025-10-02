7c4bcae1e9f318bb740f5103a70b4ff4a748d1cb
amos-babu <amos.babu@yahoo.com>
2025-10-02 16:42:13 +0300

    "File Model"

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Categories;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -9,19 +10,29 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class File extends Model
 {
     protected $fillable = [
-        "user_id",
-        "name",
-        "description",
-        "image",
-        "dxf_file",
-        "amount"
+        'user_id',
+        'name',
+        'description',
+        'image',
+        'categories',
+        'dxf_file',
+        'amount',
     ];
 
-    public function user(): BelongsTo {
+    protected function casts(): array
+    {
+        return [
+            'categories' => Categories::class,
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 
-     public function orders(): HasMany {
+    public function orders(): HasMany
+    {
         return $this->hasMany(Order::class);
     }
 }
