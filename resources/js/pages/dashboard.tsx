cd511f85dd93c1dd7fc4454c0554e932caa10de7
amos-babu <amos.babu@yahoo.com>
2025-10-11 09:20:04 +0300

    "React.Suspense"

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -3,7 +3,7 @@ import Pagination from '@/components/pagination-links';
 import AppLayout from '@/layouts/app-layout';
 import { FileProps, FlashProps, type BreadcrumbItem } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { toast, Toaster } from 'sonner';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -15,7 +15,6 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 export default function Dashboard({ files }: { files: FileProps }) {
     const { flash } = usePage<FlashProps>().props;
-    // const [items, setItems] = useState(files.data);
 
     useEffect(() => {
         if (flash.success) toast.success(flash.success);
@@ -27,7 +26,10 @@ export default function Dashboard({ files }: { files: FileProps }) {
             <Toaster richColors position="top-center" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-4">
-                    <DisplayFiles items={files.data} />
+                    <Suspense fallback={<h4 className="text-center">Loading ...</h4>}>
+                        <DisplayFiles items={files.data} />
+                    </Suspense>
+                    {/* <DisplayFiles items={files.data} /> */}
                 </div>
                 <Pagination links={files.links} />
             </div>
