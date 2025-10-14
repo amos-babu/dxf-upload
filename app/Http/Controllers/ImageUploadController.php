5c4b6b693466abeb884e9ab7ecd9f6c424793475
amos-babu <amos.babu@yahoo.com>
2025-10-14 13:06:30 +0300

    normal image upload backup code

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\FileUploadRequest;
+use App\Models\File;
+use Illuminate\Support\Facades\Auth;
 
 class FileUploadController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * @param  \App\Http\Requests\FileUploadRequest|\Illuminate\Http\Request  $request
      */
-    public function __invoke(Request $request)
+    public function __invoke(FileUploadRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data['user_id'] = Auth::id();
+
+        if ($request->hasFile('image')) {
+            $filePath = $request->file('image')->store('images', 'public');
+            $data['image'] = $filePath;
+        }
+
+        if ($request->hasFile('dxf_file')) {
+            $dxfFile = $request->file('dxf_file');
+            $dxfFileName = time().'.dxf';
+            $filePath = $dxfFile->storeAs('dxf_files', $dxfFileName, 'private');
+            $data['dxf_file'] = $filePath;
+        }
+
+        File::create($data);
+
+        return to_route('files.index')->with('success', 'Uploaded successfully');
     }
 }
