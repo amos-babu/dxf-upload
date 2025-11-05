25e005568328e15326a7c2bd8f8f061062a98511
amos-babu <amos.babu@yahoo.com>
2025-11-05 14:33:30 +0300

    skeleton rendering on loading

--- resources/js/components/display-files.tsx
+++ resources/js/components/display-files.tsx
@@ -5,23 +5,37 @@ import { Skeleton } from './ui/skeleton';
 
 export default function DisplayFiles({ posts }: { posts: FileDataProps[] }) {
     const [loaded, setLoaded] = useState(false);
+    const [loadedImages, setLoadedImages] = useState<Set<number>>(new Set());
+
+    const handleImageLoad = (id: number) => {
+        setLoadedImages((prev) => new Set(prev).add(id));
+    };
     return (
         <>
             {posts.map((post, idx) => (
                 <Link
-                    className="rounded-xl dark:hover:bg-accent hover:shadow-md/30 hover:inset-shadow-sm/30"
+                    className="rounded-xl hover:shadow-md/30 dark:hover:bg-accent"
                     key={`${post.id}-${idx}`}
                     href={route('posts.show', { post: post.id })}
                 >
-                    <figure className="max-w-lg">
-                        {!loaded && <Skeleton className="h-[300px] w-[300px] rounded-xl" />}
-                        <img className="h-auto max-w-full rounded-2xl p-2" src={post.image} alt={post.name} onLoad={() => setLoaded(true)} />
+                    <figure className="aspect-square max-w-lg overflow-hidden rounded-xl">
+                        {!loadedImages.has(post.id) && <Skeleton className="h-[300px] w-[300px] rounded-xl" />}
+                        <img
+                            className={`h-full w-full rounded-2xl object-cover transition-opacity duration-300 ${loadedImages.has(post.id) ? 'opacity-100' : 'opacity-0'}`}
+                            src={post.image}
+                            alt={post.name}
+                            onLoad={() => handleImageLoad(post.id)}
+                        />
                     </figure>
                     <h2 className="pb-2 text-center text-lg font-semibold dark:text-gray-200">
-                        {post.name || <Skeleton className="h-[20px] w-[100px] rounded-md" />}
+                        {!loadedImages.has(post.id) ? (
+                            <Skeleton className="mt-3 h-[20px] w-[100px] rounded-md text-center text-gray-300" />
+                        ) : (
+                            <p>{post.name}</p>
+                        )}
                     </h2>
                     <h4 className="pb-2 text-center text-base dark:text-gray-400">
-                        {post.description || <Skeleton className="h-[16px] w-[150px] rounded-md" />}
+                        {!loadedImages.has(post.id) ? <Skeleton className="h-[16px] w-[150px] rounded-md" /> : <p>{post.description}</p>}
                     </h4>
                 </Link>
             ))}
