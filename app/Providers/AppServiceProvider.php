2678d120cbd326b013de1ec0fe372eca4c2f7606
amos-babu <amos.babu@yahoo.com>
2026-07-04 13:10:27 +0300

    appservice https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
          if ($this->app->environment('production') || $this->app->environment('staging')) {
             URL::forceScheme('https');
         }
-        
+
         RateLimiter::for('favorites', function (Request $request) {
             return Limit::perMinute(10)
                 ->by(optional($request->user())->id ?? $request->ip())
